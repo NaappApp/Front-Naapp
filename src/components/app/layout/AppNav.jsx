ca111a67c7327b0aa9a9ef19f7d4cba91217ab29
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2024-02-14 14:52:26 +0100

    wip header menu css

--- src/components/app/layout/AppNav.jsx
+++ src/components/app/layout/AppNav.jsx
@@ -14,31 +14,41 @@ export const AppNav = () => {
             <ul className="h-full flex justify-center content-center">
                 <li className="px-2">
                     <a className="" href="/">
-                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}><img className="h-8 w-6" src={HomePicto} alt="" /></div>
+                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}>
+                            <img className="h-8 w-6" src={HomePicto} alt="" />
+                        </div>
                         <span>Accueil</span>
                     </a>
                 </li>
                 <li className="px-2">
                     <a className="" href="/">
-                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}><img className="h-8 w-10" src={EventPicto} alt="" /></div>
+                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}>
+                            <img className="h-8 w-10" src={EventPicto} alt="" />
+                        </div>
                         <span>Évènements</span>
                     </a>
                 </li>
                 <li className="px-2">
                     <a className="" href="/">
-                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}><img className="h-8 w-8" src={ResourcePicto} alt="" /></div>
+                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}>
+                            <img className="h-8 w-8" src={ResourcePicto} alt="" />
+                        </div>
                         <span>Ressources</span>
                     </a>
                 </li>
                 <li className="px-2">
                     <a className="" href="/">
-                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}><img className="h-8 w-8" src={UserPicto} alt="" /></div>
+                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}>
+                            <img className="h-8 w-8" src={UserPicto} alt="" />
+                        </div>
                         <span>Compte</span>
                     </a>
                 </li>
                 <li className="px-2">
                     <a className="" href="/">
-                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}><img className="h-8 w-10" src={MessagePicto} alt="" /></div>
+                        <div className={isActive ? "flex justify-center content-center bg-gray-100 px-2 py-1 rounded-full mb-2" : "flex justify-center content-center py-1 bg-gray-100 px-2"}>
+                            <img className="h-8 w-10" src={MessagePicto} alt="" />
+                        </div>
                         <span>Messages</span>
                     </a>
                 </li>
