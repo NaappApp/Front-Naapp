2530503ef3d7c5c3a63aeedce15f1683c874f8a3
smathis <mathis.silotia@ynov.com>
2024-03-20 13:44:03 +0100

    :lipstick:| add new banner

--- src/components/app/mock/Account.jsx
+++ src/components/app/mock/Account.jsx
@@ -7,10 +7,8 @@ import { useEffect, useState } from "react";
 
 export const Account = () => {
   const [user, setUser] = useState({});
-
-  const tokenCookie = document.cookie.split("; ")[0];
+  const tokenCookie = document.cookie.split("; ")[1].split("=")[1];
   console.log(tokenCookie);
-
   useEffect(() => {
     console.log("entered useeffect");
     const getUser = async () => {
@@ -23,11 +21,11 @@ export const Account = () => {
       });
       const data = await response.json();
       console.log("data received");
+      console.log(data);
       setUser(data);
     };
     getUser();
   }, []);
-  console.log(user);
   console.log(Object.keys(user).length);
   // const listItems = user.traits.map(
   //   (e) => (
@@ -52,68 +50,76 @@ export const Account = () => {
           <p className="Account-Header-Names">{user.username}</p>
           {/* <div className="Tag-traits">{listItems}</div> */}
         </div>
-        <button className="ModifyProfile">Modifier le profile</button>
-      </div>
-      <div className="Account-sub">
         {Object.keys(user).length !== 0 && (
           <div className="Follow">
             <FontAwesomeIcon icon={farUsers} />
             <p className="Follow-text">
-              {user.followers.length} abonné(e)s - {user.following.length} Suivi(e)s
+              {/* {user.followers.length} abonné(e)s - {user.following.length} Suivi(e)s */}
+              0 abonné(e)s - 0 Suivi(e)s
             </p>
           </div>
         )}
-        <div className="header-Bio">
-          <div className="Bio">
-            <p className="Bio-Title">Bio</p>
-            <p className="Bio-text">{user.biography}</p>
+      </div>
+      <div className="Account-sub">
+        <div className="Account-sub-menu">
+          <div className="Navigation">
+            <button className="Account-sub-menu-Item">Qui suis-je ?</button>
+            <button className="Account-sub-menu-Item">Activités</button>
+            <button className="Account-sub-menu-Item">Événement</button>
+            <button className="Account-sub-menu-Item">Paramètres</button>
           </div>
-          {Object.keys(user).length !== 0 && (
-            <div className="Interest">
-              <p className="Interest-Title">Centre d&apos;intérêt</p>
-              {/* <div className="Interest-List">{listItems2}</div> */}
+          <div className="header-Bio">
+            <div className="Bio">
+              <p className="Bio-Title">Bio</p>
+              <p className="Bio-text">{user.biography}</p>
             </div>
-          )}
-        </div>
-        <div className="Activities">
-          <div className="Menu">
-            <button className="MyPost-Title" id="MyPost-Title">
-              Mon activité
-            </button>
-            <button className="MyEvent-Title" id="MyEvent-Title">
-              Mes Événement
-            </button>
-          </div>
-          <div className="MyPost" id="MyPost">
-            <div className="MyPost-Header">
-              <p className="CreatePost">Créer un post</p>
-            </div>
-            <div className="Post">
-              <div className="Post-Header">
-                <img src={user.empty ? AccountImg : Image} alt="" className="Post-Image" />
-                <div className="Post-Header-Flex">
-                  <p className="Post-Header-Names">
-                    {user.Name} {user.FamilyName}
-                  </p>
-                  {/* <div className="Post-Header-AsdList">{listItems3}</div> */}
-                </div>
+            {Object.keys(user).length !== 0 && (
+              <div className="Interest">
+                <p className="Interest-Title">Centre d&apos;intérêt</p>
+                {/* <div className="Interest-List">{listItems2}</div> */}
               </div>
-              <p className="Post-text">{user.Bio}</p>
-            </div>
+            )}
           </div>
-          <div className="MyEvent" id="MyEvent">
-            <div className="MyEvent-Header">
-              <p className="CreateEvent">Créer un événement</p>
+          <div className="Activities">
+            <div className="Menu">
+              <button className="MyPost-Title" id="MyPost-Title">
+                Mon activité
+              </button>
+              <button className="MyEvent-Title" id="MyEvent-Title">
+                Mes Événement
+              </button>
             </div>
-            <div className="Event">
-              <div className="Event-Header">
-                <img src={user.empty ? AccountImg : Image} alt="" className="Event-Image" />
-                <div className="Event-Header-Flex">
-                  <p className="Event-Header-Names">Test</p>
-                  {/* <div className="Event-Header-AsdList">{listItems3}</div> */}
-                </div>
+            <div className="MyPost" id="MyPost">
+              <div className="MyPost-Header">
+                <p className="CreatePost">Créer un post</p>
               </div>
-              <p className="Event-text">{user.Bio}</p>
+              <div className="Post">
+                <div className="Post-Header">
+                  <img src={user.empty ? AccountImg : Image} alt="" className="Post-Image" />
+                  <div className="Post-Header-Flex">
+                    <p className="Post-Header-Names">
+                      {user.Name} {user.FamilyName}
+                    </p>
+                    {/* <div className="Post-Header-AsdList">{listItems3}</div> */}
+                  </div>
+                </div>
+                <p className="Post-text">{user.Bio}</p>
+              </div>
+            </div>
+            <div className="MyEvent" id="MyEvent">
+              <div className="MyEvent-Header">
+                <p className="CreateEvent">Créer un événement</p>
+              </div>
+              <div className="Event">
+                <div className="Event-Header">
+                  <img src={user.empty ? AccountImg : Image} alt="" className="Event-Image" />
+                  <div className="Event-Header-Flex">
+                    <p className="Event-Header-Names">Test</p>
+                    {/* <div className="Event-Header-AsdList">{listItems3}</div> */}
+                  </div>
+                </div>
+                <p className="Event-text">{user.Bio}</p>
+              </div>
             </div>
           </div>
         </div>
